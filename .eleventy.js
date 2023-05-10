91be8adbc43c209801a1c8a48ef24368a21285c6
Scott Walter <scott.walter@nice.com>
2023-05-10 14:42:47 -0500

    feat: static directories

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,13 @@
 const pluginCSS = require('eleventy-postcss-extension');
+const eleventyHelmetPlugin = require('eleventy-plugin-helmet');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginCSS);
+  eleventyConfig.addPlugin(eleventyHelmetPlugin);
+
+  eleventyConfig.addPassthroughCopy({ './content/fonts': './fonts' });
+  eleventyConfig.addPassthroughCopy({ './content/images': './images' });
+  eleventyConfig.addPassthroughCopy({ './content/static': './static' });
 
   return {
     dir: {
