202e27f6d353a6e2fffb0467ff2c900c39f4e8ac
Scott Walter <scott.walter@nice.com>
2023-05-11 12:40:51 -0500

    feat: webvitals

--- code/app.js
+++ code/app.js
@@ -1,1 +1,18 @@
+import { onCLS, onFID, onLCP, onFCP } from 'web-vitals';
+
+export const startWebVitals = () => {
+  onCLS((r) => {
+    console.log('Cumlative Layout Shift', r);
+  });
+  onFID((r) => {
+    console.log('Embedded First Input Delay', r);
+  });
+  onLCP((r) => {
+    console.log('Largest Contentful Paint', r);
+  });
+  onFCP((r) => {
+    console.log('First Contentful Paint', r);
+  });
+};
+
 export const MY_NAME = 'SCOTT Frederick WALTER';
---
+++ content/posts/another-post.md
@@ -0,0 +1,10 @@
+---
+title: This is my third post.
+description: This is a post on My Blog about win-win survival strategies.
+date: 2018-08-24
+tags:
+  - post
+  - info
+---
+
+<p>This is a test</p>
