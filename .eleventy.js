423b59a13365406876b7bf8b41f3ea5059c8867f
Scott Walter <scott.walter@nice.com>
2023-05-12 14:27:31 -0500

    feat: purge css plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const pluginCSS = require('eleventy-postcss-extension');
 const eleventyHelmetPlugin = require('eleventy-plugin-helmet');
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const purgeCssPlugin = require("eleventy-plugin-purgecss");
 
 const { DateTime } = require("luxon");
 
@@ -9,6 +10,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginCSS);
   eleventyConfig.addPlugin(eleventyHelmetPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(purgeCssPlugin, {
+    // Optional: Specify the location of your PurgeCSS config
+    config: './purgecss.config.js',
+
+    // Optional: Set quiet: true to suppress terminal output
+    quiet: false,
+  });
 
   eleventyConfig.addPassthroughCopy({ './content/fonts': './fonts' });
   eleventyConfig.addPassthroughCopy({ './content/images': './images' });
